7f8297bd0198cbbc8d4b3919f3863b497cf4dcb3
midovane <midovaneugb@gmail.com>
2022-01-29 18:05:19 +0100

    Init on git / Setup project

---
+++ src/components/InputField.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Styles.css';
+const InputField = () => {
+  return <form className='input'>
+      <input type="text"  placeholder='Enter a task' className='input__box'/>
+  <button className='input_submit' type='submit'>Go</button>
+   </form>;
+};
+
+export default InputField;
